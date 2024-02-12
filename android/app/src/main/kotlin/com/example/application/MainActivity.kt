676d6b89bb41d59bb66501b0132feb858bf133d2
Jonas <petersen0108@gmail.com>
2024-02-12 11:40:05 +0100

    Removed files so ot possible to debug on phone

--- android/app/src/main/kotlin/com/example/application/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/application/MainActivity.kt
@@ -4,27 +4,6 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-        flutterView.addFirstFrameListener {
-            if (intent.hasExtra("success")) {
-                onHCEResult(ntent)
-            }
-        }
-    }
-    
-    override fun OnNewIntent(intent: Intent?) {
-        super.OnNewIntent(intent)
-        if (intent?.hasExtra("success") == true) {
-            onHCEResult(intent)
-        }
-
-    }
-
-    private fun onHCEResult(intent: Intent) {
-        channel.invokeMethod("onHCEResult", success)
-    }
 
 
 
